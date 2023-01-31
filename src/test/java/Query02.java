2dfbb458ad62c2cc51b24db9fa2af2e9e5779573
“Mehmet <70447221+harunapak@users.noreply.github.com>
2023-01-31 22:21:25 +0100

    Second commit

--- src/test/java/Query02.java
+++ src/test/java/Query02.java
@@ -116,9 +116,3 @@ public class Query02 {
 
     }
 }
-    git init
-    git add README.md
-        git commit -m "first commit"
-        git branch -M main
-        git remote add origin https://github.com/Mhmthrn/JDBCGrup.git
-        git push -u origin main
